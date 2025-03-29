935f3b352abe2dcd003b844e379e5b9049626393
Colourless <205353678+AcrylonitrileButadieneStyrene@users.noreply.github.com>
2025-03-29 09:21:38 -0400

    fix build script patching wrong file

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 nix develop .flake --command cargo build --release
-nix run nixpkgs#patchelf -- --set-interpreter "$(nix eval nixpkgs#stdenv.cc.bintools.dynamicLinker --raw)" ./target/release/ynotwitch
+nix run nixpkgs#patchelf -- --set-interpreter "$(nix eval nixpkgs#stdenv.cc.bintools.dynamicLinker --raw)" ./target/release/controller
